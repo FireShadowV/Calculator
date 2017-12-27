7616455041076e615572014d4fb800dd07bc5c23
FireShadowV <mutambeshyajustin@gmail.com>
2017-12-27 23:12:58 +0100

    Replaced If statements with single switch statement

--- Comeback/src/Start.java
+++ Comeback/src/Start.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+
 
 public class Start {
 	public static void main(String args[]) {
@@ -11,44 +13,85 @@ public class Start {
 		Scanner reader = new Scanner(System.in);	
 		System.out.println("Type a number to choose the operation : 1. Optelllen  2. Aftrekken  3. Vermenigvuldigen  4. Delen 5. Eind");
 		OperationNumber = reader.nextInt();
-			
-		if(OperationNumber == 1) {
+		
+		switch(OperationNumber) {
+		case 1:
 			System.out.println("Enter the first number: ");
-			int myNumber = reader.nextInt();
+		    double myNumber = reader.nextInt();
 			System.out.println("Enter the second number: ");
-			int myNumber2 = reader.nextInt();
-			int result = myNumber + myNumber2;
+			double myNumber2 = reader.nextInt();
+			double result1 = myNumber + myNumber2;
+			System.out.println("The result is: " + result1);
+			break;
+		case 2:
+			System.out.println("Enter the first number: ");
+			double myNumber3 = reader.nextInt();
+			System.out.println("Enter the second number: ");
+			double myNumber4 = reader.nextInt();
+			double result2 = myNumber3 - myNumber4;
+			System.out.println("The result is: " + result2);
+			break;
+		case 3:
+			System.out.println("Enter the first number: ");
+			double myNumber5 = reader.nextInt();
+			System.out.println("Enter the second number: ");
+			double myNumber6 = reader.nextInt();
+			double result3 = myNumber5 * myNumber6;
+			System.out.println("The result is: " + result3);
+			break;
+		case 4:
+			System.out.println("Enter the first number: ");
+			double myNumber7 = reader.nextInt();
+			System.out.println("Enter the second number: ");
+			double myNumber8 = reader.nextInt();
+			double result4 = myNumber7 / myNumber8;
+			System.out.println("The result is: " + result4);
+			break;
+		
+		}
+		
+		
+		
+		
+		
+		
+		/*if(OperationNumber == 1) {
+			System.out.println("Enter the first number: ");
+		    double myNumber = reader.nextInt();
+			System.out.println("Enter the second number: ");
+			double myNumber2 = reader.nextInt();
+			double result = myNumber + myNumber2;
 			System.out.println("The result is: " + result);
 			
 			
 		} else if (OperationNumber == 2) {
 			System.out.println("Enter the first number: ");
-			int myNumber = reader.nextInt();
+			double myNumber = reader.nextInt();
 			System.out.println("Enter the second number: ");
-			int myNumber2 = reader.nextInt();
-			int result = myNumber - myNumber2;
+			double myNumber2 = reader.nextInt();
+			double result = myNumber - myNumber2;
 			System.out.println("The result is: " + result);
 			
 			
 		} else if (OperationNumber == 3) {
 			System.out.println("Enter the first number: ");
-			int myNumber = reader.nextInt();
+			double myNumber = reader.nextInt();
 			System.out.println("Enter the second number: ");
-			int myNumber2 = reader.nextInt();
-			int result = myNumber * myNumber2;
+			double myNumber2 = reader.nextInt();
+			double result = myNumber * myNumber2;
 			System.out.println("The result is: " + result);
 			
 			
 		}else if (OperationNumber == 4) {
 			System.out.println("Enter the first number: ");
-			int myNumber = reader.nextInt();
+			double myNumber = reader.nextInt();
 			System.out.println("Enter the second number: ");
-			int myNumber2 = reader.nextInt();
-			int result = myNumber / myNumber2;
+			double myNumber2 = reader.nextInt();
+			double result = myNumber / myNumber2;
 			System.out.println("The result is: " + result);
 			
 			
-		} 
+		} */
 		
 		}while(OperationNumber != 5);
 		
